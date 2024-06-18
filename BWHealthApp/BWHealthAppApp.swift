304b16e68faf1d49ff0382b6e279914ab606587f
Himalaya <himalaya@bornwest.com>
2024-06-18 19:05:49 +0530

    Initial Commit

---
+++ BWHealthApp/BWHealthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BWHealthAppApp.swift
+//  BWHealthApp
+//
+//  Created by Himalaya Rajput on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct BWHealthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
